31b4ef72ec79490edd21835c4bac21c9c0f2eb4f
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-14 02:31:56 +0530

    Create string_01.py

---
+++ string_01.py
@@ -0,0 +1,17 @@
+string="The sun sets slowly behind the quiet hills."
+
+print(string.endswith("lls."))
+print(string.endswith("hello"))
+
+print(string.startswith("The s"))
+print(string.startswith("hills"))
+
+print(string.capitalize())
+count=string.count("s")
+
+index=string.find("hills.")
+print(index)
+
+replaced_string=string.replace("slowly","gentally")
+print(replaced_string)
+
