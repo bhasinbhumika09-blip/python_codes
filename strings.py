b9415ff61293174500c013b778dddb03051928b2
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-13 23:13:50 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,11 +1,12 @@
 a="Heyyy!! How are you?"
 
 print(a)
-print(a[0])
-print(a[3])
-print(a[9])
-print(a[14])
 print(len(a))
-print(a[1:10])
+print(a[0])
+print(a[9])
+print(a[15])
+print(a[2:14])
+print(a[:10])
 print(a[4:])
-print(a[:11])
+print(a[-9:-2])
+print(a[:-7])
