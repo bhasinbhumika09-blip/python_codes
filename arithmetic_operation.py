35c59cf29f36a0b95d3d55d6a4941e1c7776f700
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-13 22:18:39 +0530

    Create arithmetic_operation.py

---
+++ arithmetic_operation.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter a number:"))
+num2=int(input("Enter another number:"))
+
+print("Addition of two numbers is:",num1+num2)
+print("Substraction of two numbers is:",num1-num2)
+print("Multiplication of two numbers is:",num1*num2)
+print("Division of two numbers is:",num1/num2)
+print("Reminder of two numbers is:",num1%num2)
+print("Power to the number is:",num1**num2)
