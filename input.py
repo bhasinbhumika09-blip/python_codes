cadf1efe7cdf9d03c5d60530d2008af4f4093315
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-13 21:14:18 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+a=input("Enter something:")
+#by default, whatever is added inside the input operation its is of string datatype
+print(a)
+
+b=int(input("Enter any number:"))
+#it helps to change the datatype to integer type now we can enter any integer value to it
+print(b)
