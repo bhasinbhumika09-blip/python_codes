5dd00d74d8eb482172d118613205a0cdaf034bd9
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-14 02:38:55 +0530

    Create escape_sequence_character.py

---
+++ escape_sequence_character.py
@@ -0,0 +1,11 @@
+print("Hello \n everyone!!!!!")
+# \n ---- next line
+
+print("Hello \t everyone!!!!!")
+# \t ---- tab space
+
+print("Hello \' everyone!!!!!")
+# \' ---- single quote
+
+print("Hello \\ everyone!!!!!")
+# \\ ---- backslash
