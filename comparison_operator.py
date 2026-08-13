c8b865ed61a1f6e187e8b276ef3109a779371f4e
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-13 23:00:25 +0530

    Create comparison_operator.py

---
+++ comparison_operator.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter a number:"))
+num2=int(input("Enter another number:"))
+
+print("First number is greater than second number:",num1>num2)
+print("First number is greater than or equal to second number:",num1>=num2)
+print("Second number is greater than first number:",num1<num2)
+print("First number is greater than or equal to second number:",num1<=num2)
+print("Both numbers are equal:",num1==num2)
+print("Both numbers are not equal:",num1!=num2)
+
