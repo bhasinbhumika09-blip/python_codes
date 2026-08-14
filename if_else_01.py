b4e047552d1e43efaf1cf261faf870a33282952f
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-14 12:00:26 +0530

    Add if-else statements to evaluate number properties

---
+++ if_else_01.py
@@ -0,0 +1,16 @@
+num = 10
+
+if num > 0:
+    print("Number is positive")
+else:
+    print("Number is not positive")
+
+if num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+if num > 5:
+    print("Number is greater than 5")
+else:
+    print("Number is 5 or less")
