c265c6a199b80466191834b3a0ff25bcfffa15a6
Bhumika Bhasin <bhasinbhumika09@gmail.com>
2026-08-14 10:34:18 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,22 @@
+list_1=[1,6,3,7,2,32,65,98,72,56,12]
+
+list_1.sort() # use to arrange numbers in ascending order
+print(list_1)
+
+list_1.reverse()    # use to arrange numbers in reverse order
+print(list_1)
+
+list_1.insert(321,6)  # use to insert any number at that index
+print(list_1)
+
+list_1.append(90)    # use  to insert any number at last
+print(list_1)
+
+list_1.pop(6)   # use to delete 6th index element
+print(list_1)
+
+list_1.remove(65)    # use to delete that element
+print(list_1)
+
+list_1.sort()
+print(list_1)
